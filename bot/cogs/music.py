3c0fd186d00ccf75b8809667b5076aeb26e8e66b
Nemika <33802693+Nemijus@users.noreply.github.com>
2021-02-20 16:18:47 +0200

    Added some songs and finished the game owo

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -1,10 +1,12 @@
-import discord, youtube_dl, asyncio, json
+import discord, youtube_dl, asyncio, json, random
 
 from youtube_search import YoutubeSearch
 
-from core import checks, embeds
+from core import checks, embeds, files
 
 from discord.ext import tasks
+
+from difflib import SequenceMatcher
 
 commands = discord.ext.commands
 
@@ -52,7 +54,61 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
 class Music(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot         
+        self.bot = bot
+
+    def similar(self, s_1, s_2): return SequenceMatcher(None, s_1, s_2).ratio() > 0.6
+
+    @commands.guild_only()
+    @commands.command()
+    async def play(self, ctx):
+        song = random.choice(files.Data("songs").json_read())
+
+        player = await YTDLSource.from_url(song, loop=self.bot.loop)
+
+        title = player.title.lower()
+
+        ctx.voice_client.play(player)
+
+        await ctx.send(embed=discord.Embed(
+            title="Guess The Song!",
+            description="Now playing track! Try to guess the song before it's over!",
+            color=discord.Color.green()
+        ))
+
+        try:
+            answer = await self.bot.wait_for(
+                "message",
+                timeout=player.data['duration'],
+                check=lambda message: self.similar(message.content.lower(), title)
+            )
+
+        except asyncio.TimeoutError:
+            await ctx.voice_client.disconnect()
+            return await ctx.send(embed=discord.Embed(
+                title="Song is over!",
+                description=f"Nobody guessed the song! It was `{player.title}`!",
+                color=discord.Color.red(),
+                url=player.data['webpage_url']
+            )
+            .set_thumbnail(url=player.data['thumbnail']))
+
+        await ctx.voice_client.disconnect()
+        return await ctx.send(embed=discord.Embed(
+                title="Congratulations!",
+                description=f"{answer.author.mention} guessed the song! It was `{player.title}`!",
+                color=discord.Color.green(),
+                url=player.data['webpage_url']
+            )
+            .set_thumbnail(url=player.data['thumbnail']))
+    
+    @play.before_invoke
+    async def ensure_voice(self, ctx):
+        if not ctx.voice_client:
+            if not ctx.author.voice:
+                return await ctx.send(embed=embeds.Embeds("You must be connected in a voice channel!"))
+            else:
+                await ctx.author.voice.channel.connect()
+
 
 def setup(bot):
     bot.add_cog(Music(bot))
